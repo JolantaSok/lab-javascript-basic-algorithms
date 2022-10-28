5ba0c6cac24ceb506bc920fbf343d2e3b1eca099
JolantaSok <jolanta.vingre@inbox.lv>
2022-10-28 10:55:03 +0200

    dont know how to do 3.part.

--- js/index.js
+++ js/index.js
@@ -2,5 +2,34 @@
 //
 // Iteration 2: Conditionals
 
-
 // Iteration 3: Loops
+
+let hacker1 = "Jolanta";
+console.log("The driver's name is " + hacker1);
+
+let hacker2 = "Atvars";
+console.log("The navigator's name is " + hacker2);
+
+let driversname = hacker1.length
+let navigatorsname = hacker2.length
+
+if (hacker1.length > hacker2.length) {
+  console.log("The driver has the longest name, it has " + driversname + " characters");
+} else {
+  if (hacker1.length < hacker2.length) {
+    console.log("It seems that the navigator has the longest name, it has " + navigatorsname + " characters.");
+  } else {
+    console.log("Wow, you both have equally long names, " + driversname || navigatorsname + " characters!");
+  }
+}
+
+console.log(hacker1.toUpperCase().split('').join(' '));
+
+console.log(hacker2.split('').reverse().join(''))
+
+for (let i = 0; i <= 100; i++) {
+    console.log("The driver's name goes first.");
+  }
+
+
+
