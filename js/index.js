08952b7ed88275197c787ac5be858ba239f5df2a
JolantaSok <jolanta.vingre@inbox.lv>
2022-10-28 19:45:43 +0200

    Done 3.part :)

--- js/index.js
+++ js/index.js
@@ -10,26 +10,42 @@ console.log("The driver's name is " + hacker1);
 let hacker2 = "Atvars";
 console.log("The navigator's name is " + hacker2);
 
-let driversname = hacker1.length
-let navigatorsname = hacker2.length
+let driversname = hacker1.length;
+let navigatorsname = hacker2.length;
 
 if (hacker1.length > hacker2.length) {
-  console.log("The driver has the longest name, it has " + driversname + " characters");
+  console.log(
+    "The driver has the longest name, it has " + driversname + " characters"
+  );
 } else {
   if (hacker1.length < hacker2.length) {
-    console.log("It seems that the navigator has the longest name, it has " + navigatorsname + " characters.");
+    console.log(
+      "It seems that the navigator has the longest name, it has " +
+        navigatorsname +
+        " characters."
+    );
   } else {
-    console.log("Wow, you both have equally long names, " + driversname || navigatorsname + " characters!");
+    console.log(
+      "Wow, you both have equally long names, " + driversname ||
+        navigatorsname + " characters!"
+    );
   }
 }
 
-console.log(hacker1.toUpperCase().split('').join(' '));
+console.log(hacker1.toUpperCase().split("").join(" "));
 
-console.log(hacker2.split('').reverse().join(''))
-
-for (let i = 0; i <= 100; i++) {
-    console.log("The driver's name goes first.");
-  }
+console.log(hacker2.split("").reverse().join(""));
 
 
-
+while (hacker1 < hacker2) {
+  console.log("The driver's name goes first.");
+  break;
+}
+while (hacker1 > hacker2) {
+  console.log("Yo, the navigator goes first definitely.");
+  break;
+}
+while (hacker1 == hacker2) {
+  console.log("What?! You both have the same name?");
+  break;
+}
